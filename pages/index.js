7e3a1e6a4d9635c2252ad791109b0cd464385bfe
Shylexx <alexanderdauncey@gmail.com>
2022-01-21 03:06:02 +0000

    Initial Commit

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from "@chakra-ui/react"
+
+const Page = () => {
+    return(
+        <Container>
+            <Box borderRadius="lg" bg="nb" p={3} align="center">
+                Hello, I&apos;m a second year Student at the University of Brighton, UK.
+            </Box>
+        </Container>
+    )
+}
+
+export default Page
