eff031f0687e55bccf2f6b67f6f12a33497f65fa
Alex Dauncey <alexander@dauncey.net>
2022-01-21 04:40:28 +0000

    Homepage Template

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
-import { Container, Box } from "@chakra-ui/react"
+import { Container, Box, Heading } from "@chakra-ui/react"
 
 const Page = () => {
     return(
         <Container>
-            <Box borderRadius="lg" bg="nb" p={3} align="center">
+            <Box borderRadius="lg" bg="red" p={3} align="center">
                 Hello, I&apos;m a second year Student at the University of Brighton, UK.
+            </Box>
+
+            <Box display={{md: 'flex'}}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Alexander Dauncey
+                    </Heading>
+                    <p>Game Developer ( Design / Programming )</p>
+                </Box>
             </Box>
         </Container>
     )
